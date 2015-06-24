9d97c1d299cb2a44b7293a989aa1173232b26576
David Tulig <dtulig@indeed.com>
2015-06-24 11:57:21 -0500

    Add documentation for setting up purescript-mode.
    Purescript mode has a few setup steps to byte compile and then create
    autoloads. Added a comment with a git link and some tricks for compiling
    it on a mac.

--- .emacs.d/src/purescript.el
+++ .emacs.d/src/purescript.el
@@ -1,3 +1,6 @@
+;; https://github.com/dysinger/purescript-mode
+;; make EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs all
+;; M-x update-directory-autoloads
 (add-to-list 'load-path "~/.emacs.d/non-elpa/purescript-mode")
 (require 'purescript-mode-autoloads)
 (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
