a3795be9fb0341aaa2b493419e6bd8e92ade11cd
David Tulig <david.tulig@gmail.com>
2017-05-08 08:10:06 -0500

    xmonad: give xmonad and xmobar a nicer font and icons.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -19,19 +19,33 @@ main = do
        , normalBorderColor = "#073642"
        , focusedBorderColor = "#cb4b16"
        , startupHook = setWMName "LG3D"
+       , workspaces = myWorkspaces
        , handleEventHook = docksEventHook <+> fullscreenEventHook <+> handleEventHook def
        , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook def
        , layoutHook = avoidStruts $ myLayout
        , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
-            , ppCurrent = xmobarColor "#b58900" "" . wrap "[" "]"
+            , ppCurrent = xmobarColor "#cb3b16" "" 
+            , ppVisible = xmobarColor "#b58900" ""
             , ppTitle = xmobarColor "#cb4b16" "" . shorten 50
             , ppLayout = const "" -- to disable the layout info on xmobar
+            , ppSep = " :  "
             }
        } `additionalKeys`
-       [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-       , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'xft:inconsolata:size=10:antialias=true' -nb '#002b36'")
+       [ ((mod4Mask .|. shiftMask, xK_z), spawn "/home/dtulig/bin/lock_screen.sh")
+       --, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'xft:Fira Mono Medium for Powerline:pixelsize=18:antialias=true' -nb '#002b36'")
+       , ((mod4Mask, xK_p), spawn "rofi -show run")
        ]
+
+myWorkspaces = [ " 1 \xf269 "
+               , "2 \xf121 "
+               , "3 \xf121 "
+               , "4 "
+               , "5 \xf11b"
+               , "6 "
+               , "7 "
+               , "8 "
+               , "9 " ]
 
 myLayout = tiled ||| Mirror tiled ||| Full
   where
